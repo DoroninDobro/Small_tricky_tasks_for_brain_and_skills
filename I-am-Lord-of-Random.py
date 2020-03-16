38ec4ee968f91e7d316ecc85233f46b23dd4e0f2
Vovka <59031665+DoroninDobro@users.noreply.github.com>
2020-03-16 19:49:19 +0200

    7kuy CodeWars

---
+++ I-am-Lord-of-Random.py
@@ -0,0 +1,17 @@
+from random import randint,seed
+seed(1)
+guess = randint(1,100)
+seed(1)
+
+
+'''Fun Task from CodeWars, I know principe randint() =))
+
+The test fixture I use for this kata is pre-populated.
+
+It will compare your guess to a random number generated using:
+
+randint(1,100) You can pass by relying on luck or skill but try not to rely on luck.
+
+"The power to define the situation is the ultimate power." - Jerry Rubin
+
+Good luck!'''
